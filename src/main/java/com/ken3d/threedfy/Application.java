0529c831b3bc8566be4eb198ca64d0d9d450bf34
guidou44 <guim_44@hotmail.com>
2020-05-03 12:53:29 -0400

    changed target AWS env in CD

--- src/main/java/com/ken3d/threedfy/Application.java
+++ src/main/java/com/ken3d/threedfy/Application.java
@@ -2,8 +2,10 @@ package com.ken3d.threedfy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.ken3d.threedfy.infrastructure.dal.entities")
 public class Application {
 
   public static void main(String[] args) {
